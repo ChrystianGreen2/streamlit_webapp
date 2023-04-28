607053f543bf073bec2856d5c2c476de6290dfcb
ChrystianGreen2 <jose.lima.4616@gmail.com>
2023-04-28 01:14:37 -0300

    third commit

--- main.py
+++ main.py
@@ -1,46 +1,17 @@
 import streamlit as st
-import qrcode
 import sqlite3
-from PIL import Image
+import qrcode
 import io
+from PIL import Image
+
+st.set_page_config(
+    page_title="NFC Authentication",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
 DB_NAME = "users.db"
-
-def create_table():
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, email TEXT, nfc_id TEXT UNIQUE)''')
-    conn.commit()
-    conn.close()
-
-def create_user(name, email, nfc_id):
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute("INSERT INTO users (name, email, nfc_id) VALUES (?, ?, ?)", (name, email, nfc_id))
-    conn.commit()
-    conn.close()
-
-def get_users():
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute("SELECT * FROM users")
-    users = c.fetchall()
-    conn.close()
-    return users
-
-def update_user(user_id, name, email):
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute("UPDATE users SET name = ?, email = ? WHERE id = ?", (name, email, user_id))
-    conn.commit()
-    conn.close()
-
-def delete_user(user_id):
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute("DELETE FROM users WHERE id = ?", (user_id,))
-    conn.commit()
-    conn.close()
 
 def get_user_by_nfc_id(nfc_id):
     conn = sqlite3.connect(DB_NAME)
@@ -50,81 +21,107 @@ def get_user_by_nfc_id(nfc_id):
     conn.close()
     return user
 
+def create_user(name, email, phone, address, nfc_id):
+
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute("INSERT INTO users (name, email, phone, address, nfc_id) VALUES (?, ?, ?, ?, ?)", (name, email, phone, address, nfc_id))
+    conn.commit()
+    conn.close()
+
+def create_table():
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        phone TEXT NOT NULL,
+        address TEXT NOT NULL,
+        nfc_id TEXT NOT NULL
+    )''')
+    conn.commit()
+    conn.close()
+
 def pil_image_to_bytes(img):
     byte_stream = io.BytesIO()
     img.save(byte_stream, format='PNG')
     img_bytes = byte_stream.getvalue()
     return img_bytes
 
-def generate_qrcode(nfc_id):
+def generate_qrcode(url):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
-    qr.add_data(nfc_id)
+    qr.add_data(url)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
     return img
 
 create_table()
 
-st.title("Gerenciador de Usuários")
+st.markdown("""
+<style>
+    .header {
+        display: flex;
+        align-items: center;
+    }
+    .header img {
+        height: 80px;
+    }
+    .header h1 {
+        font-size: 48px;
+        margin-left: 10px;
+    }
+</style>
+""", unsafe_allow_html=True)
 
-menu = st.sidebar.selectbox(
-    "Menu",
-    [
-        "Listar Usuários",
-        "Adicionar Usuário",
-        "Atualizar Usuário",
-        "Remover Usuário",
-        "Buscar Usuário por ID do Cartão",
-        "Gerar QRCode",
-    ],
-)
+st.markdown("""
+<div class="header">
+    <img src="https://geticom.uema.br/static/media/header-logo.bba4ba401721d8fd6422.png" />
+</div>
+""", unsafe_allow_html=True)
 
-if menu == "Listar Usuários":
-    users = get_users()
-    st.write(users)
+st.title("Bem vindo")
 
-elif menu == "Adicionar Usuário":
-    name = st.text_input("Nome")
-    email = st.text_input("Email")
-    nfc_id = st.text_input("ID do Cartão NFC")
-    if st.button("Adicionar Usuário"):
-        create_user(name, email, nfc_id)
-        st.write("Usuário adicionado com sucesso!")
+menu = st.sidebar.selectbox("Menu", ["Informações do Usuário", "Gerar QR Code"])
 
-elif menu == "Atualizar Usuário":
-    user_id = st.number_input("ID do Usuário", value=None, format="%i")
-    name =  st.text_input("Nome")
-    email = st.text_input("Email")
-    if st.button("Atualizar Usuário"):
-        update_user(user_id, name, email)
-        st.write("Usuário atualizado com sucesso!")
-
-elif menu == "Remover Usuário":
-    user_id = st.number_input("ID do Usuário", value=None, format="%i")
-    if st.button("Remover Usuário"):
-        delete_user(user_id)
-        st.write("Usuário removido com sucesso!")
-
-elif menu == "Buscar Usuário por ID do Cartão":
-    nfc_id = st.text_input("ID do Cartão NFC")
-    if st.button("Buscar Usuário"):
+if menu == "Informações do Usuário":
+    nfc_id = st.experimental_get_query_params().get("nfc_id")
+    if nfc_id:
+        nfc_id = nfc_id[0]
         user = get_user_by_nfc_id(nfc_id)
-    if user:
-        st.write("ID do Usuário:", user[0])
-        st.write("Nome:", user[1])
-        st.write("Email:", user[2])
+        if user:
+            st.subheader("Informações do Usuário")
+            st.write(f"**ID:** {user[0]}")
+            st.write(f"**Nome:** {user[1]}")
+            st.write(f"**Email:** {user[2]}")
+            st.write(f"**Telefone:** {user[3]}")
+            st.write(f"**Endereço:** {user[4]}")
+            
+            # Adicione campos adicionais aqui, se necessário
+        else:
+            st.info('Nenhum usuário encontrado com este ID de cartão NFC.', icon="ℹ️")
+            st.info("Por favor, cadastre-se abaixo.")
+            name = st.text_input("Nome")
+            email = st.text_input("Email")
+            phone = st.text_input("Telefone")
+            address = st.text_input("Endereço")
+            # Adicione campos adicionais aqui, se necessário
+            if st.button("Cadastrar Usuário"):
+                create_user(name, email, phone, address, nfc_id)  # Atualize a função create_user para aceitar campos adicionais
+                st.success('Usuário cadastrado com sucesso!', icon="✅")
     else:
-        st.write("Nenhum usuário encontrado com este ID de cartão NFC.")
+        st.write("Seja bem-vindo à página de autenticação via NFC.")
 
-elif menu == "Gerar QRCode":
-    nfc_id = st.text_input("ID do Cartão NFC")
-    if st.button("Gerar QRCode"):
-        img = generate_qrcode(nfc_id)
+elif menu == "Gerar QR Code":
+    base_url = "https://chrystiangreen2-streamlit-webapp-main-vp911m.streamlit.app/"
+    nfc_id = st.text_input("Digite o ID do cartão NFC:")
+    if st.button("Gerar QR Code"):
+        url = f"{base_url}?nfc_id={nfc_id}"
+        img = generate_qrcode(url)
         img_bytes = pil_image_to_bytes(img)
-        st.image(img_bytes, caption="QRCode", use_column_width=True, output_format='PNG')
-
+        st.image(img_bytes, caption="QR Code", use_column_width=True, output_format='PNG')
